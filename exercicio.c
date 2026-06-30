c3e17d0b39a9ad2a3cf08e56036a8d0b7512abdd
Vinícius José Orsi <vinicius.orsi@edu.univali.br>
2026-06-30 18:02:38 -0300

    falta deixar bonito

--- exercicio.c
+++ exercicio.c
@@ -1,55 +1,76 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #define tam 15
+#define notas 3
+#define alunos 2
 
 int main () {
 
 //variaveis
 
-char aluno_1[tam];
-char aluno_2[tam];
-char aluno_3[tam];
-char aluno_4[tam];
-char aluno_5[tam];
-int num_alunos = 0;
+char nome_alunos[alunos][tam];
+float matriz_nota[alunos][notas];
+int num_alunos = 1;
+float media_maior = 0;
+int save;
 
-float a1_n1, a1_n2, a1_n3;
-float a2_n1, a2_n2, a2_n3;
-float a3_n1, a3_n2, a3_n3;
-float a4_n1, a4_n2, a4_n3;
-float a5_n1, a5_n2, a5_n3;
-
-float media_a1;
-float media_a2;
-float media_a3;
-float media_a4;
-float media_a5;
-
-char resultado[tam];
-
-//apresentação
-printf("Calculadora de Médias\n");
-printf("Digite o nome do aluno 1: ");
-scanf("%s", &aluno_1);
-
-
-
-if (media_a1 < 7) {
-    resultado[tam] = "Reprovado";
-} else {
-    resultado[tam] = "Aprovado";
+float media[alunos];
+for (int i = 0; i<alunos; i++) {
+    media[i] = 0;
 }
 
-printf("ALUNO         N1    N2    N3    MÉDIA    SITUAÇÃO\n");
-printf("%s         %.1f    %.1f    %.1f    %.1f    %s\n", aluno_1, a1_n1, a1_n2, a1_n3, media_a1, resultado);
+char situacao[alunos][tam];
 
+//codigo
 
+printf("Calculadora de Médias\n");
 
+for (int i = 0; i<alunos; i++) {
 
-
-
-
-
-
+    printf("Digite o nome do aluno %d: ", i+1);
+    scanf("%s", nome_alunos[i]);
     
+
+    for (int j = 0; j<notas; j++) {
+        do {
+            printf("Digite a nota %d de %s: ", j+1, nome_alunos[i]);
+            scanf("%f", &matriz_nota[i][j]);
+            if (matriz_nota[i][j] < 0 || matriz_nota[i][j] > 10){
+                printf("NOTA INVALIDA: Digite um numero de 0 a 10.\n");
+            }    
+        }
+        while (matriz_nota[i][j] < 0 || matriz_nota[i][j] > 10);
+    }
+
+    system("cls");
+
+    for (int j = 0; j<notas; j++) {
+        media[i] += matriz_nota[i][j];
+    }
+    media[i] /= notas;
+
+    if (media[i]>media_maior) {
+        media_maior = media[i];
+        save = i;
+    }
+
+    if (media[i]<7) {
+        strcpy(situacao[i], "Reprovado");
+    } else {
+        strcpy(situacao[i], "Aprovado");
+    }
+
+    num_alunos++;
+}
+
+printf("ALUNO        N1     N2     N3     MEDIA      SITUACAO\n");
+for (int i = 0; i<alunos; i++) {
+    printf("%s        %.1f     %.1f     %.1f     %.1f      %s\n", nome_alunos[i], matriz_nota[i][0], matriz_nota[i][1], matriz_nota[i][2], media[i], situacao[i]);
+}
+
+printf("\n\nMaior media: %.1f  -  %s", media[save], nome_alunos[save]);
+
+
     return 0;
 }
